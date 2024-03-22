9ebb121a9026d5a951b17312725929dcb2e8027f
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2024-03-21 21:45:10 -0300

    cria o projeto

---
+++ src/main/java/br/ufrn/imd/incluevents/InclueventsApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.incluevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InclueventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InclueventsApplication.class, args);
+	}
+
+}

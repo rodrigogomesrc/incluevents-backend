7c6a8ff2a503eee25ef82c55b18ac13fcc478c1e
Ana Carolina <anacarolinaneta@hotmail.com>
2024-04-20 11:31:31 -0300

    corrigindo mineração do ingresse

--- src/main/java/br/ufrn/imd/incluevents/config/Setup.java
+++ src/main/java/br/ufrn/imd/incluevents/config/Setup.java
@@ -2,6 +2,7 @@ package br.ufrn.imd.incluevents.config;
 
 import br.ufrn.imd.incluevents.model.Evento;
 import br.ufrn.imd.incluevents.service.EventoService;
+import br.ufrn.imd.incluevents.service.IngresseScraperImpl;
 import br.ufrn.imd.incluevents.service.OutgoScraperImpl;
 import br.ufrn.imd.incluevents.service.SymplaScraperImpl;
 
@@ -16,13 +17,15 @@ public class Setup implements ApplicationRunner {
 
     private final OutgoScraperImpl outgoScraper;
     private final SymplaScraperImpl SymplaScraperImpl;
+    private final IngresseScraperImpl ingresseScraper;
     private final EventoService eventoService;
 
 
-    public Setup(OutgoScraperImpl outgoScraper, EventoService eventoService, SymplaScraperImpl SymplaScraperImpl) {
+    public Setup(OutgoScraperImpl outgoScraper, EventoService eventoService, SymplaScraperImpl SymplaScraperImpl, IngresseScraperImpl ingresseScraper) {
         this.outgoScraper = outgoScraper;
         this.eventoService = eventoService;
         this.SymplaScraperImpl = SymplaScraperImpl;
+        this.ingresseScraper = ingresseScraper;
     }
 
     @Override
@@ -36,5 +39,12 @@ public class Setup implements ApplicationRunner {
         //System.out.println("Eventos buscados: ");
         //eventos.forEach(System.out::println);
         //this.eventoService.saveAll(eventos);
+
+        //Ingresse{
+        //List<Evento> ingresseEventos = ingresseScraper.scrape();
+        //System.out.println("Eventos Ingresse buscados: ");
+        //ingresseEventos.forEach(System.out::println);
+        //this.eventoService.saveAll(ingresseEventos);
+        //}
     }
 }

d1c188316e69cca71e7629a2efa70978a0d011c8
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2024-07-07 12:12:29 -0300

    adiciona maracanaScraper

--- src/main/java/br/ufrn/imd/incluevents/framework/config/Setup.java
+++ src/main/java/br/ufrn/imd/incluevents/framework/config/Setup.java
@@ -1,37 +1,18 @@
 package br.ufrn.imd.incluevents.framework.config;
 
-import br.ufrn.imd.incluevents.framework.model.Evento;
-import br.ufrn.imd.incluevents.framework.service.EventoService;
 //import br.ufrn.imd.incluevents.natal.service.IngresseScraperImpl;
 //import br.ufrn.imd.incluevents.natal.service.OutgoScraperImpl;
 //import br.ufrn.imd.incluevents.natal.service.SymplaScraperImpl;
 
-import br.ufrn.imd.incluevents.ufpb.service.UfpbScraperImpl;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
 public class Setup implements ApplicationRunner {
 
-    //private final OutgoScraperImpl outgoScraper;
-    //private final SymplaScraperImpl SymplaScraperImpl;
-    //private final IngresseScraperImpl ingresseScraper;
-    private final EventoService eventoService;
 
-    /*
-    public Setup(OutgoScraperImpl outgoScraper, EventoService eventoService, SymplaScraperImpl SymplaScraperImpl, IngresseScraperImpl ingresseScraper) {
-        this.outgoScraper = outgoScraper;
-        this.eventoService = eventoService;
-        this.SymplaScraperImpl = SymplaScraperImpl;
-        this.ingresseScraper = ingresseScraper;
-    }
-     */
-
-    public Setup(EventoService eventoService) {
-        this.eventoService = eventoService;
+    public Setup() {
     }
 
     @Override
@@ -51,5 +32,9 @@ public class Setup implements ApplicationRunner {
 //        ingresseEventos.forEach(System.out::println);
 //        this.eventoService.saveAll(ingresseEventos);
           //List<Evento> ufpbEventos = ufpbScraper.scrape();
+
+        //System.out.println("Eventos Maracanã buscados: ");
+        //List<Evento> evntos = maracanaScraper.scrape();
+        //evntos.forEach(System.out::println);
     }
 }
